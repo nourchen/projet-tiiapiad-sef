3d17affbcd65a00dff8cd8c9fa8956ae5aff29c4
sylviaTVieira <sylviaTVieira@gmail.com>
2011-10-14 22:32:27 +0000

    Petite demo de ce qu'on peut faire avec JfreeChart...
    Pour le moment c'est une demo chopée sur le net, je vais bosser a l'appliquer a un SEF créé...

--- SEF/src/ihm/FenetreGeometrique.java
+++ SEF/src/ihm/FenetreGeometrique.java
@@ -1,10 +1,27 @@
 package ihm;
+
+
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 /**
  * Classe représentant un repère, permettant le rendu graphique d'un sous ensemble flou
  * @author Sylvia Vieira
  *
  */
-public class FenetreGeometrique {
+
+
+/**
+ * A simple demo showing a dataset created using the {@link XYSeriesCollection} class.
+ *
+ */
+public class FenetreGeometrique extends ApplicationFrame {
 
 	//A priori => utiliser XY Plot de JFreeChart?
 	
@@ -16,4 +33,44 @@ public class FenetreGeometrique {
 	 * =>Permettre à l'utilisateur de voir les coordonnées d'un point s'afficher lors du passage du cursuer dessus
 	 * (quelque part dans la fenêtre, peu importe ou, à définir plus tard...)
 	 */
+	
+	
+
+
+
+    /**
+     * A demonstration application showing an XY series containing a null value.
+     *
+     * @param title  the frame title.
+     */
+    public FenetreGeometrique(final String title) {
+
+        super(title);
+        final XYSeries series = new XYSeries("Random Data");
+        series.add(1.0, 500.2);
+        series.add(5.0, 694.1);
+        series.add(4.0, 100.0);
+        series.add(12.5, 734.4);
+        series.add(17.3, 453.2);
+        series.add(21.2, 500.2);
+        series.add(21.9, null);
+        series.add(25.6, 734.4);
+        series.add(30.0, 453.2);
+        final XYSeriesCollection data = new XYSeriesCollection(series);
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "XY Series Demo",
+            "X", 
+            "Y", 
+            data,
+            PlotOrientation.VERTICAL,
+            true,
+            true,
+            false
+        );
+
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(chartPanel);
+
+    }
 }
--- SEF/src/ihm/Launcher.java
+++ SEF/src/ihm/Launcher.java
@@ -1,13 +1,23 @@
 package ihm;
 
+import org.jfree.ui.RefineryUtilities;
+
 public class Launcher {
 
 	/**
 	 * @param args
 	 */
-	
+
 	public static void main(String[] args) {
-		 FenetrePrincipale fp = new FenetrePrincipale();	
-		}
+		FenetrePrincipale fp = new FenetrePrincipale();
+		
+		//Demo
+		final FenetreGeometrique demo = new FenetreGeometrique("XY Series Demo");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+
+	}
 
 }
