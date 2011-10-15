d5547bcea4e7e61367f6aca0ffe9c9207faa9e0b
sylviaTVieira <sylviaTVieira@gmail.com>
2011-10-15 20:40:18 +0000

    Ajout d'un SEFManager a remplir (ce que je suis en train de faire pour le moment) Ajout de qq commentaires de-ci de-là

--- SEF/src/ihm/FenetreGeometrique.java
+++ SEF/src/ihm/FenetreGeometrique.java
@@ -24,53 +24,54 @@ import org.jfree.ui.RefineryUtilities;
 public class FenetreGeometrique extends ApplicationFrame {
 
 	//A priori => utiliser XY Plot de JFreeChart?
-	
+
 	//Pour le moment on se contentera d'un graphique simple.
-	
+
 	/*Idées "si on a le temps":
 	 * =>afficher plusieurs "courbes" (en fait une infinité de droites) sur le même graphique
 	 *         avec couleur et légendes associées
-	 * =>Permettre à l'utilisateur de voir les coordonnées d'un point s'afficher lors du passage du cursuer dessus
+	 * =>Permettre à l'utilisateur de voir les coordonnées d'un point s'afficher lors du passage du curseur dessus
 	 * (quelque part dans la fenêtre, peu importe ou, à définir plus tard...)
 	 */
-	
-	
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * A demonstration application showing an XY series containing a null value.
+	 *
+	 * @param title  the frame title.
+	 */
+	public FenetreGeometrique(final String title) {
 
-    /**
-     * A demonstration application showing an XY series containing a null value.
-     *
-     * @param title  the frame title.
-     */
-    public FenetreGeometrique(final String title) {
+		super(title);//title=titre de la FENETRE!!
+		final XYSeries series = new XYSeries("Random Data");//Titre de la serie (s'affiche dans la legende)
+		series.add(1.0, 500.2);
+		series.add(5.0, 694.1);
+		series.add(4.0, 100.0);
+		series.add(12.5, 734.4);
+		series.add(17.3, 453.2);
+		series.add(21.2, 500.2);
+		series.add(21.9, null);
+		series.add(25.6, 734.4);
+		series.add(30.0, 453.2);
+		final XYSeriesCollection data = new XYSeriesCollection(series);
+		final JFreeChart chart = ChartFactory.createXYLineChart(
+				"BLA",//Le titre du graphique
+				"X", //titre des abscisses
+				"Y", //titre des ordonnees
+				data,//La collection de XYSeries a afficher
+				PlotOrientation.VERTICAL,
+				true,  //Show legend
+				true, // Tools Tips
+				false  //urls
+				);
 
-        super(title);
-        final XYSeries series = new XYSeries("Random Data");
-        series.add(1.0, 500.2);
-        series.add(5.0, 694.1);
-        series.add(4.0, 100.0);
-        series.add(12.5, 734.4);
-        series.add(17.3, 453.2);
-        series.add(21.2, 500.2);
-        series.add(21.9, null);
-        series.add(25.6, 734.4);
-        series.add(30.0, 453.2);
-        final XYSeriesCollection data = new XYSeriesCollection(series);
-        final JFreeChart chart = ChartFactory.createXYLineChart(
-            "XY Series Demo",
-            "X", 
-            "Y", 
-            data,
-            PlotOrientation.VERTICAL,
-            true,
-            true,
-            false
-        );
+		final ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+		setContentPane(chartPanel);
 
-        final ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
-        setContentPane(chartPanel);
-
-    }
+	}
 }
--- SEF/src/manipSef/SEF.java
+++ SEF/src/manipSef/SEF.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 public class SEF {
 	/**
 	 * borne inférieure de l'intervalle de définition du sous ensemble flou
+	 * Pour l'affichage dans l'interface, sinon useless...
 	 */
 	private double borneInf;
 	/**
@@ -22,7 +23,20 @@ public class SEF {
 	 */
 	private ArrayList<Point> inflexions;
 	
+	/*
+	 * a actualiser a la volee lors du remplissage des points dans le sef
+	 * servira ensuite pour savoir quels sont les min et max (vraies bornes finalement) d'entre tous les sef rentrés
+	 */
+	private double minX;
+	private double maxX;
+	
 	public SEF(double inf,double sup,ArrayList<Point> listePoints){
+		borneInf = inf;
+		borneSup = sup;
+		inflexions=listePoints;
+	}
+	
+	public SEF(double inf, double sup){//preferable, mieux vaut ajouter les points au fur et a mesure de la lecture du fichier
 		borneInf = inf;
 		borneSup = sup;
 	}
@@ -43,9 +57,24 @@ public class SEF {
 		borneSup = sup;
 	}
 
-	//Il faudra penser à ordonner la liste (en fct des abscisses) à un moment donné 
-	//=> où? PB de modélisation, à voir plus tard!
-	public void ajouterPointInflexion(Point p){
+	/*
+	 * Il faudra penser à ordonner la liste (en fct des abscisses) à un moment donné 
+	 * => où? PB de modélisation, à voir plus tard!
+	 * 
+	 * Au pire (selon MJ Lesot) preciser dans les sec qu'une liste ordonnee est attendue
+	 * Sinon, avantage des XYSeries=>il me semble qu'ils ordonnent automatiquement les points par les x...
+	 */
+	
+	public void ajouterPointsInflexions(Point p){
+		//Modification des min et max a la volee
+		if(inflexions.isEmpty()){
+			
+			this.minX=p.getX();
+			this.maxX=p.getX();
+		}else{
+			minX=Math.min(minX, p.getX());
+			maxX=Math.min(maxX, p.getX());
+		}
 		inflexions.add(p);
 	}	
 	
