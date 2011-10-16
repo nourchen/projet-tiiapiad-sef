c797b2facff599693df263e91ca6853652639e3b
sylviaTVieira@gmail.com <sylviaTVieira@gmail.com@75719599-de6f-838f-a1ec-eb576554d30d>
2011-10-16 01:54:58 +0000

    THE methode qui m'a pris tout ce temps: createmySefCollection
    Pas encore testée.... Moment de vérité est arrivé!!!

--- trunk/SEF/src/manipSef/SefManager.java
+++ trunk/SEF/src/manipSef/SefManager.java
@@ -2,6 +2,9 @@ package manipSef;
 
 import java.util.ArrayList;
 
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 
 /**
  * *La classe depuis laquelle on pourra
@@ -14,13 +17,51 @@ import java.util.ArrayList;
 public class SefManager {
 
 	private ArrayList<SEF> mesSef;
-	private double minX;
-	private double maxX;
+	private int minX;
+	private int maxX;
+	private final int nbValDiscretes;
 	
 	public SefManager(ArrayList<SEF> mS,double min, double max){
 		this.mesSef=mS;
-		//Double.
+		this.maxX=(int)Math.ceil(max);
+		this.minX=(int)Math.floor(min);		
+		nbValDiscretes=100;
+	}
+	
+	private double getPasX(){
+		return (maxX -minX)/(nbValDiscretes-1);
+	}
+	
+	public XYSeriesCollection createMySefCollection(){
+		XYSeriesCollection myListOfSefs=new XYSeriesCollection();
+		double pas=this.getPasX();
+		int noSef=1;
 		
-		
+		for(SEF sef:mesSef){
+			//ATTENTION!! Je pars du principe que le sef est ordonné par x croissant!!
+			final XYSeries courbe = new XYSeries("Sous ensemble"+noSef);
+			double a, b, xDroite, xGauche;
+			int indicePoint;
+			for(indicePoint =0;indicePoint<sef.getInflexions().size()-1;indicePoint++){
+				//Je recupere les deux points consecutifs qui caracterisent la portion de droite
+				Point pgauche=sef.getInflexions().get(indicePoint);
+				Point pdroit=sef.getInflexions().get(indicePoint+1);
+				xGauche=pgauche.getX();
+				xDroite=pdroit.getX();//me servira pour savoir si je suis encore dans la portion de droite consideree
+				
+				//Je stocke les coefficients caracteristiques de l'equation de droite
+				a=(pgauche.getY()-pdroit.getY())/(pgauche.getX()-xDroite);
+				b=pdroit.getY()-a*pdroit.getX();
+				do{
+					courbe.add(xGauche, xGauche*a +b);
+					xGauche+= pas;
+					
+				}while(xGauche<xDroite);
+				
+			}
+			noSef++;
+			myListOfSefs.addSeries(courbe);
+		}
+		return myListOfSefs;
 	}
 }
