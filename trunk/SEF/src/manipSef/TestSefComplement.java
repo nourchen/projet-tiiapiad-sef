7b65d1520d718e0a838a82e0d08b7650ca6d2dbf
sylviaTVieira@gmail.com <sylviaTVieira@gmail.com@75719599-de6f-838f-a1ec-eb576554d30d>
2011-10-24 20:31:00 +0000

    Complémentaire fonctionne

--- trunk/SEF/src/manipSef/TestSefComplement.java
+++ trunk/SEF/src/manipSef/TestSefComplement.java
@@ -2,7 +2,7 @@ package manipSef;
 
 import ihm.FenetreGeometrique;
 
-import java.util.ArrayList;
+
 
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -11,23 +11,24 @@ import org.jfree.ui.RefineryUtilities;
 public class TestSefComplement {
 
 	public static void main(String[] args) {
-		XYSeries sef1 = new XYSeries("sef1");
-		XYSeries sef2 = new XYSeries("sef2");
+		SEF sef1 = new SEF(-30, 60, new XYSeries("sef1"));
+		SEF sef2 = new SEF(-30, 60, new XYSeries("sef2"));
 		 
-		sef1.add(-1.5,0);
-		sef1.add(1.25,1);
-		sef1.add(4.2,0.75);
-		sef1.add(5,0);
+		sef1.getInflexions().add(-1.5,0);
+		sef1.getInflexions().add(1.25,1);
+		sef1.getInflexions().add(4.2,0.75);
+		sef1.getInflexions().add(5,0);
 		
-		sef2.add(-0.5,0);
-		sef2.add(0,1);
-		sef2.add(0.5,0);
+		sef2.getInflexions().add(-0.5,0);
+		sef2.getInflexions().add(0,1);
+		sef2.getInflexions().add(0.5,0);
 		
 		
 		XYSeriesCollection mesSefs= new XYSeriesCollection();
-		mesSefs.addSeries(sef1);
-		mesSefs.addSeries(sef2);
-		mesSefs.addSeries(sef1);
+		mesSefs.addSeries(sef1.getInflexions());
+		mesSefs.addSeries(sef2.getInflexions());
+		mesSefs.addSeries(SefComplement.getComplement(sef1).getInflexions());
+		mesSefs.addSeries(SefComplement.getComplement(sef2).getInflexions());
 		
 		FenetreGeometrique frame = new FenetreGeometrique("Manipulation des Sous Ensembles Flous", mesSefs);
 		//La string passée en param du constructeur est le titre de la fenetre
