3ca9d08748947d6a86d5336bea444e0bf02e240c
sylviaTVieira@gmail.com <sylviaTVieira@gmail.com@75719599-de6f-838f-a1ec-eb576554d30d>
2011-10-27 21:34:12 +0000

    La fonction de normalisation a l'air de marcher.
    J'ai essayé sur 5 cas différents, j'en ai chié pour que tous les exemples passent la validation!!!
    
    Je vais faire encore quelques tests sur des exemples différents histoire d'etre sure....

--- trunk/SEF/src/tests/TestNormalize.java
+++ trunk/SEF/src/tests/TestNormalize.java
@@ -22,21 +22,29 @@ public class TestNormalize {
 		sef2.getInflexions().add(0,1);
 		sef2.getInflexions().add(0.5,0);
 		
-		sef3.getInflexions().add(3,0);
-		sef3.getInflexions().add(4,1);
-		sef3.getInflexions().add(5,0);
+		sef2.getInflexions().add(3,0);
+		sef2.getInflexions().add(4,1);
+		sef2.getInflexions().add(5,0);
+		sef2.getInflexions().add(6, 0.7);
 		
 		sef1.printInflexions();
 		sef2.printInflexions();
+		//sef3.printInflexions();
 		System.out.println("Apres avoir fait la normalisation: \n");
 		
-		//SefIntersection.normalizeSerie(sef1.getInflexions(),sef1.getBorneInf(),sef1.getBorneSup(),
-		//		sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup());
+//		SefIntersection.normalizeSerie(sef1.getInflexions(),sef1.getBorneInf(),sef1.getBorneSup(),
+//			sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup());
 		SefIntersection.normalizeSerie(sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup(),
 					sef1.getInflexions(),sef1.getBorneInf(),sef1.getBorneSup());
+		
+//		SefIntersection.normalizeSerie(sef3.getInflexions(),sef3.getBorneInf(),sef3.getBorneSup(),
+//				sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup());
+//		SefIntersection.normalizeSerie(sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup(),
+//				sef3.getInflexions(),sef3.getBorneInf(),sef3.getBorneSup());
+		
 		sef1.printInflexions();
 		sef2.printInflexions();
-		
+		//sef3.printInflexions();
 		
 	}
 	
