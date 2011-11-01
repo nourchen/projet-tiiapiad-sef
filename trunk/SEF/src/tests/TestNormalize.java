e072504893898bdc804c887808329871f84c5bdd
sylviaTVieira@gmail.com <sylviaTVieira@gmail.com@75719599-de6f-838f-a1ec-eb576554d30d>
2011-11-01 18:28:19 +0000

    Correction du bug des bornes dans la fonction de normalisation
    + Début de test Intersection Zadeh

--- trunk/SEF/src/tests/TestNormalize.java
+++ trunk/SEF/src/tests/TestNormalize.java
@@ -17,7 +17,7 @@ import exceptions.NormalizationException;
 public class TestNormalize {
 
 	public static void main(String[] args) {
-
+		/*
 		SEF sef1 = new SEF(-30, 60, new XYSeries("sef1"));
 		SEF sef2 = new SEF(-30, 60, new XYSeries("sef2"));
 		SEF sef3 = new SEF(-30, 60, new XYSeries("sef3"));
@@ -26,12 +26,18 @@ public class TestNormalize {
 		sef1.getInflexions().add(-1.5,0);
 		sef1.getInflexions().add(1.25,1);
 		sef1.getInflexions().add(4.2,0.75);
-		sef1.getInflexions().add(5,0);
+		sef1.getInflexions().add(5,0);*/
 		/*
 		sef2.getInflexions().add(-0.5,0.6);
 		sef2.getInflexions().add(0,1);
 		sef2.getInflexions().add(0.5,0);
 		 */
+
+		/*		sef4.getInflexions().add(-0.5,0.6);
+		sef4.getInflexions().add(0,0.8);
+		sef4.getInflexions().add(0.5,0);
+
+
 		sef2.getInflexions().add(3,0);
 		sef2.getInflexions().add(4,1);
 		sef2.getInflexions().add(5,0);
@@ -54,7 +60,7 @@ public class TestNormalize {
 //		sef3.printInflexions();
 //		sef4.printInflexions();
 		System.out.println("Apres avoir fait la normalisation: \n");
-
+		 */
 		//		SefIntersection.normalizeSerie(sef1.getInflexions(),sef1.getBorneInf(),sef1.getBorneSup(),
 		//			sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup());
 		//		SefIntersection.normalizeSerie(sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup(),
@@ -65,6 +71,20 @@ public class TestNormalize {
 		//		SefIntersection.normalizeSerie(sef2.getInflexions(),sef2.getBorneInf(),sef2.getBorneSup(),
 		//				sef3.getInflexions(),sef3.getBorneInf(),sef3.getBorneSup());
 
+		SEF sef3 = new SEF(-30, 60, new XYSeries("sef3"));
+		SEF sef4 = new SEF(-30, 0, new XYSeries("sef4"));
+
+		sef4.getInflexions().add(-3,0);
+		sef4.getInflexions().add(0,1);
+		//sef4.getInflexions().add(1,1);
+
+		sef3.getInflexions().add(-6, 0);
+		sef3.getInflexions().add(-4, 0.75);
+		sef3.getInflexions().add(2, 0.75);
+		sef3.getInflexions().add(5, 0.75);
+		
+		sef3.printInflexions();
+		sef4.printInflexions();
 		try {
 			SefIntersection.normalizeSerie(sef3.getInflexions(),sef3.getBorneInf(),sef3.getBorneSup(),
 					sef4.getInflexions(),sef4.getBorneInf(),sef4.getBorneSup());
@@ -76,17 +96,21 @@ public class TestNormalize {
 			System.out.println(e.getErrorMsg());
 		}
 
+//		sef3.printInflexions();
+//		sef4.printInflexions();
 		//sef1.printInflexions();
 		//sef2.printInflexions();
-		finally{
-			//sef3.printInflexions();
-			//sef4.printInflexions();
-		}
-		
+//		finally{
+//			//sef3.printInflexions();
+//			//sef4.printInflexions();
+//		}
+
+
+
 		/*
 		 * Essai de la copie d'une liste XYSeries, celle ci a l'air de fonctionner bien
 		 *  ====>sera utile plus tard!!!!
-		 */
+		 *//*
 		XYSeries listePts5 ;
 		try {
 			listePts5 = sef1.getInflexions().createCopy(0,sef1.getInflexions().getItemCount()-1);
@@ -102,9 +126,9 @@ public class TestNormalize {
 			//  Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("Il y a eu un souci dans la copie de la liste de points du sef "
-								+sef1.getInflexions().getKey());
+					+sef1.getInflexions().getKey());
 		}
-		
+*/
 	}
 
 }
