90edade8a53f75f6479a3556bcdd0fd0ccb7bd23
sylviaTVieira <sylviaTVieira@gmail.com>
2011-11-04 23:16:19 +0000

    Fonction Cos terminée ainsi que la fonction sin

--- SEF/src/principeExtension/Extension.java
+++ SEF/src/principeExtension/Extension.java
@@ -1,7 +1,42 @@
 package principeExtension;
 
+import org.jfree.data.xy.XYSeries;
+
+import exceptions.UnknownFunctionException;
+import manipSef.SEF;
+
 public class Extension {
-	
-	
+
+	private IMapping func;
+	private double inf;
+	private double sup;
+	private SEF sef;
+
+	public Extension(SEF sef,double Xinf,double Xsup, FunctionChoice f) throws UnknownFunctionException {
+		switch (f) {
+		case COS:
+			this.func = new FunctionCosinus(Xinf,Xsup); 
+			break;
+		case SQUARE:
+			this.func = new FunctionSquare();
+			break;
+		default:
+			throw new UnknownFunctionException();
+		}
+		this.inf=Xinf;
+		this.sup=Xsup;
+		this.sef = sef;
+	}
+
+
+	public SEF getExtendedSef(){
+		XYSeries ptsSefImage = new XYSeries(sef.getInflexions().getKey()+ " étendu par la fonction "+func.toString());
+
+		//On discrétise le sef sur l'intervalle donné
+		
+		//Tout d'abord on se crée une liste d'image par mapping de compute sur l'ensemble des x discrétisés
+
+		return new SEF(inf, sup, ptsSefImage);
+	}
 
 }
