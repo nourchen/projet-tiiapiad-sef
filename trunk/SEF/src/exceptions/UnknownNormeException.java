aecfc04f72e8bb09e6f54bc76e14d984ea8613ec
sylviaTVieira@gmail.com <sylviaTVieira@gmail.com@75719599-de6f-838f-a1ec-eb576554d30d>
2011-11-05 23:54:19 +0000

    Ajout de doc pour package exceptions

--- trunk/SEF/src/exceptions/UnknownNormeException.java
+++ trunk/SEF/src/exceptions/UnknownNormeException.java
@@ -2,7 +2,7 @@ package exceptions;
 
 /**
  * Exception générée lorque l'on a cherche a effectuer une opération 
- * avec une T-norme (ou T-conorme) qui n'est pas connue
+ * avec une T-norme (ou T-conorme) qui n'est pas connue (en dehors du catalogue -enum)
  * 
  * @author Sylvia Vieira
  *
@@ -10,7 +10,7 @@ package exceptions;
 public class UnknownNormeException extends Exception{
 
 	/**
-	 * 
+	 * identifiant série
 	 */
 	private static final long serialVersionUID = 994526847035443909L;
 
